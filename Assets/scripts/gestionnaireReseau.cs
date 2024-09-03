ea1e46ee3ebbaa9f26e3d5d22d9988bfb5a5c1ad
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-09-03 10:16:28 -0400

    Initial commit

---
+++ Assets/scripts/gestionnaireReseau.cs
@@ -0,0 +1,196 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+public class gestionnaireReseau : MonoBehaviour, INetworkRunnerCallbacks
+{
+    //Contient une référence au component NetworkRunner
+    NetworkRunner _runner;
+    // pour mémoriser le component GestionnaireMouvementPersonnage du joueur
+    gestionnaireInputs gestionnaireInputs;
+    //Index de la scène du jeu
+    public int IndexSceneJeu;
+    // Contient la référence au script JoueurReseau du Prefab
+    public joueurReseau joueurPrefab;
+    // Tableau de couleurs à définir dans l'inspecteur
+    public Color[] couleurJoueurs;
+    // Pour compteur le nombre de joueurs connectés
+    public int nbJoueurs = 0;
+
+    void Start()
+    {
+        // Création d'une partie dès le départ
+        //CreationPartie(GameMode.AutoHostOrClient);
+    }
+
+    // Fonction asynchrone pour démarrer Fusion et créer une partie
+    public async void CreationPartie(GameMode mode)
+    {
+        /*  1.Mémorisation du component NetworkRunner . On garde en mémoire
+            la référence à ce component dans la variable _runner.
+            2.Indique au NetworkRunner qu'il doit fournir les entrées (inputs) au 
+            simulateur (Fusion)
+        */
+        _runner = gameObject.GetComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+
+        /*Méthode du NetworkRunner qui permet d'initialiser une partie
+         * GameMode : reçu en argument. Valeur possible : Client, Host, Server,
+           AutoHostOrClient, etc.)
+         * SessionName : Nom de la chambre (room) pour cette partie
+         * Scene : la scène qui doit être utilisée pour la simulation
+         * SceneManager : référence au component script
+          NetworkSceneManagerDefault qui est ajouté au même moment
+         */
+        await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "Chambre test",
+            Scene = SceneRef.FromIndex(IndexSceneJeu),
+            PlayerCount = 10, //ici, on limite à 10 joueurs
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+       
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        
+    }
+    /* Fonction du Runner pour définir les inputs du client dans la simulation
+     * 1. On récupère le component GestionnaireInputs du joueur local
+     * 2. On définit (set) le paramètre input en lui donnant la structure de données (struc) qu'on récupère
+     * en appelant la fonction GestInputReseau du script GestionnaireInputs. Les valeurs seront mémorisées
+     * et nous pourrons les utilisées pour le déplacement du joueur dans un autre script. Ouf...*/
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        //1.
+        if (gestionnaireInputs == null && joueurReseau.Local != null)
+        {
+
+            gestionnaireInputs = joueurReseau.Local.GetComponent<gestionnaireInputs>();
+        }
+
+        //2.
+        if (gestionnaireInputs != null)
+        {
+            input.Set(gestionnaireInputs.GetInputReseau());
+        }
+
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+       
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        if (_runner.IsServer)
+        {
+            Debug.Log("Un joueur s'est connecté comme serveur. Spawn d'un joueur");
+            /*On garde la référence au nouveau joueur créé par le serveur. La variable locale
+             créée est de type JoueurReseau (nom du script qui contient la fonction Spawned()*/
+            joueurReseau leNouveuJoueur = _runner.Spawn(joueurPrefab, utilitaires.GetPositionSpawnAleatoire(),
+                                            Quaternion.identity, player);
+            /*On change la variable maCouleur du nouveauJoueur et on augmente le nombre de joueurs connectés
+            Comme j'ai seulement 10 couleurs de définies, je m'assure de ne pas dépasser la longueur de mon
+            tableau*/
+            Debug.Log(nbJoueurs);
+            leNouveuJoueur.maCouleur = couleurJoueurs[nbJoueurs];
+            nbJoueurs++;
+            if (nbJoueurs >= 10) nbJoueurs = 0;
+        }
+        else
+        {
+            Debug.Log("Un joueur s'est connecté comme client. Spawn d'un joueur");
+        }
+}
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+       
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        /*
+         * Fonction appelée lorsqu'une connexion réseau est refusée ou lorsqu'un client perd
+        * la connexion suite à une erreur réseau. Le paramètre ShutdownReason est une énumération (enum)
+        * contenant différentes causes possibles.
+         */
+        if (shutdownReason == ShutdownReason.GameIsFull)
+        {
+            Debug.Log("Le maximum de joueur est atteint. Réessayer plus tard.");
+        }
+
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        
+    }
+}
---
+++ Assets/scripts/joueurReseau.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Fusion; // namespace pour utiliser les classes de Fusion
+/* 
+ * 1.Les objets réseau ne doivent pas dériver de MonoBehavior, mais bien de NetworkBehavior
+ * Importation de l'interface IPlayerLeft
+ * 2.Variable pour mémoriser l'instance du joueur
+ * 3.Fonction Spawned() : Semblable au Start(), mais pour les objets réseaux
+ * Sera exécuté lorsque le personnage sera créé (spawn)
+ * Test si le personnage créé est le personnage contrôlé par l'utilisateur local.
+ * HasInputAuthority permet de vérifier cela.
+ * Retourne true si on est sur le client qui a généré la création du joueur
+ * Retourne false pour les autres clients
+ * 4. Lorsqu'un joueur se déconnecte du réseau, on élimine (Despawn) son joueur.
+ */
+public class joueurReseau : NetworkBehaviour, IPlayerLeft //1.
+{
+    //Variable qui sera automatiquement synchronisée par le serveur sur tous les clients
+    [Networked] public Color maCouleur { get; set; }
+    // Variable pour le pointage (nombre de boules rouge) du joueur qui sera automatiquement synchronisé par le serveur sur tous les clients
+    // Lorsqu'un chanegement est détecté, la fonction OnChangementPointage sera automatiquement appelée pour faire
+    // une mise à jour de l'affichage du texte.
+    [Networked, OnChangedRender(nameof(OnChangementPointage))] public int nbBoulesRouges { get; set; }
+    //Variable réseau (Networked) contenant le nom du joueur (sera synchronisée)
+    [Networked] public string monNom { get; set; }
+    // Variable pour mémoriser la zone de texte au dessus de la tête du joueur et qui afficher le pointage
+    // Cette variable doit être définie dans l'inspecteur de Unity
+    public TextMeshProUGUI affichagePointageJoueur;
+    public static joueurReseau Local; //.2
+    public Transform modeleJoueur;
+
+    /*
+     * Au départ, on change la couleur du joueur. La variable maCouleur sera définie
+     * par le serveur dans le script GestionnaireReseau.La fonction Start() sera appelée après la fonction Spawned().
+     */
+    private void Start()
+    {
+        GetComponentInChildren<MeshRenderer>().material.color = maCouleur;
+    }
+
+    public override void Spawned() //3.
+    {
+        // À sa création, le joueur ajoute sa référence (son script JoueurReseau) et son pointage (var nbBoulesRouges) au dictionnaire
+        // du GameManager.
+        GameManager.joueursPointagesData.Add(this, nbBoulesRouges);
+
+        if (Object.HasInputAuthority)
+        {
+            Local = this;
+            Debug.Log("Un joueur local a été créé");
+            /*À la création du joueur et s'il est le joueur local (HasInputAuthority), ont doit défénir son nom en allant
+          chercher la variable nomJoueurLocal du GameManager.
+          Pour que le nom soit synchronisé sur tous les clients, appelle d'une fonction RPC (RemoteProcedureCall) qui permet
+          de dire à tous les clients d'exécuter la fonction  "RPC_ChangementdeNom"
+          */
+            monNom = GameManager.nomJoueurLocal;
+            RPC_ChangementdeNom(monNom);
+
+            //Si c'est le joueur du client, on appel la fonction pour le rendre invisible
+            utilitaires.SetRenderLayerInChildren(modeleJoueur, LayerMask.NameToLayer("JoueurLocal"));
+
+            //On désactive la mainCamera. Assurez-vous que la caméra de départ possède bien le tag MainCamera
+            Camera.main.gameObject.SetActive(false);
+        }
+        else
+        {
+            //Si le joueur créé est contrôlé par un autre joueur, on désactive le component caméra de cet objet
+            Camera camLocale = GetComponentInChildren<Camera>();
+            camLocale.enabled = false;
+
+            // On désactive aussi le component AudioListener
+            AudioListener audioListener = GetComponentInChildren<AudioListener>();
+            audioListener.enabled = false;
+            Debug.Log("Un joueur réseau a été créé");
+        }
+        // on affiche le nom du joueur créé et son pointage
+        affichagePointageJoueur.text = $"{monNom}:{nbBoulesRouges.ToString()}";
+    }
+
+    public void PlayerLeft(PlayerRef player) //.4
+    {
+        if (player == Object.InputAuthority)
+        {
+            Runner.Despawn(Object);
+        }
+    }
+
+    /* Fonction appelée automatiquement lorsqu'un changement est détecté dans la variable nbBoulesRouges du joueur (variable Networked)
+    Mise à jour du pointage du joueur qui sera égal au nombre de boules rouges ramassées
+*/
+    public void OnChangementPointage()
+    {
+        affichagePointageJoueur.text = $"{monNom}:{nbBoulesRouges.ToString()}";
+    }
+
+    /* Fonction RPC (RemoteProcedureCall) déclenché par un joueur local qui permet la mise à jour du nom du joueur
+    sur tous les autres clients. La source (l'émetteur) est le joueur local (RpcSources.InputAuthority). La cible est tous les joueurs
+    connectés (RpcTargets.All). Le paramètre reçu contient le nom du joueur à défénir.
+    Pour bien comprendre : Mathieu se connecte au serveur en inscrivant son nom. Il envoir un message à tous les autres clients. Sur
+    chaque client, le joueur contrôlé par Mathieu exécutera cette fonction ce qui permettra une mise à jour du nom.
+    1. On définit la variable nomNom
+    2. On affiche le nom et le poitage au dessus de la tête du joueur.
+    */
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+    public void RPC_ChangementdeNom(string leNom, RpcInfo infos = default)
+    {
+        //1.
+        monNom = leNom;
+        //2.
+        affichagePointageJoueur.text = $"{monNom}:{nbBoulesRouges.ToString()}";
+    }
+}
+
